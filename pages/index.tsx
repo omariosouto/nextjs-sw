b95abb989f6a2aa54c817912fb6727f927e7286b
Mario Souto <13791385+omariosouto@users.noreply.github.com>
2023-07-11 23:47:42 -0300

    update

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,24 @@
+import React from "react";
+
 export default function Home() {
+  React.useEffect(() => {
+    console.log("Hello, world!");
+    fetch("/api/", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        "X-SW-Cache": "true",
+        "X-SW-Expires": "60",
+        "X-SW-Retry": "1",
+      },
+    })
+      .then((respostaDoServidor) => {
+        return respostaDoServidor.json();
+      })
+      .then((respostaCompleta) => {
+        console.log(respostaCompleta);
+      });
+  }, []);
+
   return <div>Welcome to Next.js!</div>
 }
--- public/sw.js
+++ public/sw.js
@@ -5,3 +5,49 @@ self.addEventListener('install', () => {
 self.addEventListener('activate', () => {
   console.log('service worker activated')
 });
+
+
+self.addEventListener('fetch', function (event) {
+  var url = new URL(event.request.url);
+  const BASE_URL = url.origin + url.pathname;
+
+  if (BASE_URL.includes("/api")) {
+    const cache = event.request.headers.get('X-SW-Cache');
+    const retry = event.request.headers.get('X-SW-Retry');
+    const expires = event.request.headers.get('X-SW-Expires');
+    console.log("[cache]", cache);
+    console.log("[retry]", retry);
+    console.log("[expires]", expires);
+    if(cache === 'true') {
+      event.respondWith(
+        caches.match(event.request)
+          .then(function (response) {
+            if (response) {
+              return response;
+            }
+            return fetch(event.request);
+          })
+      );
+      return;
+    }
+
+
+    event.respondWith(
+      fetch(event.request)
+        .then(res => {
+          if (res.status === 200) {
+            const clonedRes = res.clone();
+            caches.open('sw-proxy-cache-v1')
+              .then(cache => {
+                cache.put(event.request, clonedRes);
+              });
+          }
+          
+          return res;
+        })
+        .catch(err => {
+          console.error('Error fetching from GitHub API: ', err);
+        })
+    );
+  }
+});
